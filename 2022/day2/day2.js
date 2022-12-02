19e08ad066b8cf8f4229947823300a9bf9ce5c60
JIMO_17_ <jies1702@gmail.com>
2022-12-02 16:03:17 -0500

    Tweak/ change let by const

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -3,7 +3,7 @@ const holidays = ['01/06', '04/01', '12/25']
 
 function countHours(year, holidays) {
     const validDays = [1, 2, 3, 4, 5];
-    let hours_ = holidays.map(holiday => new Date(`${year}/${holiday}`).getDay());
+    const hours_ = holidays.map(holiday => new Date(`${year}/${holiday}`).getDay());
 
     const hours = hours_.filter(hour => validDays.includes(hour));
     return hours.reduce((acc, cur) => acc + 2, 0);
