dd4958d9ac47894742eac19d9f192d948fe30e7f
JIMO_17_ <jies1702@gmail.com>
2022-12-02 16:19:40 -0500

    Improve v2 solution challenge day 2

--- 2022/day2/day2.js
+++ 2022/day2/day2.js
@@ -3,14 +3,10 @@ const holidays = ['01/06', '04/01', '12/25']
 
 function countHours(year, holidays) {
     const validDays = [1, 2, 3, 4, 5];
-    let hours = 0;
   
-    holidays
-      .map(holiday => new Date(`${year}/${holiday}`).getDay())
-      .filter(hour => validDays.includes(hour))
-      .map((c) => hours+=2)
-
-    return hours
+    return holidays
+      .filter(holiday => validDays.includes(new Date(`${year}/${holiday}`).getDay()))
+      .reduce((acc, cur) => acc + 2, 0)
 }
 
 countHours(year, holidays) 
