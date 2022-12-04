fe9830b004dc1533cac61c61e1e87ad69e921beb
Josue Isai Mesa Mora <57819435+JIMO-17@users.noreply.github.com>
2022-12-04 02:17:08 +0000

    day 3 solved

---
+++ 2022/day3/day3.js
@@ -0,0 +1,14 @@
+const packOfGifts = ["book", "doll", "ball"]
+const reindeers = ["dasher", "dancer"]
+
+distributeGifts(packOfGifts, reindeers)
+
+function distributeGifts(packOfGifts, reindeers) {
+    const weigths = packOfGifts.map((pack) => pack.length);
+  
+    const totalWeigth = weigths.reduce((acc, ac) => acc += ac, 0);
+  
+    const carryReindeers = reindeers.reduce((acc, ac) => acc + (ac.length * 2), 0);
+  
+    return parseInt(carryReindeers / totalWeigth)
+}
