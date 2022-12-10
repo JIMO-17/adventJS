5f095924b6c57f119bc550178d1313d1d6d1dd9d
JIMO_17_ <jies1702@gmail.com>
2022-12-09 19:19:02 -0500

    improve solution for day 8

--- 2022/day8/day8.js
+++ 2022/day8/day8.js
@@ -7,6 +7,7 @@ checkPart("miidim"); // true
 checkPart("midu"); // false
 // "midu" cannot be a palindrome after removing a character
 
+// first way 40 points
 function checkPart(part) {
   if (part === part.split("").reverse().join("")) return true;
 
@@ -16,4 +17,26 @@ function checkPart(part) {
   }
 
   return false;
+}
+
+// second way - 240 points
+function checkPart2(part) {
+  const medio = part.length / 2;
+
+  for (let i = 0; i < medio; i++) {
+    const j = part.length - i - 1;
+    if (part[i] !== part[j]) {
+      return (
+        part.slice(i, j) === part.slice(i, j).split("").reverse().join("") ||
+        part.slice(i + 1, j + 1) ===
+          part
+            .slice(i + 1, j + 1)
+            .split("")
+            .reverse()
+            .join("")
+      );
+    }
+  }
+
+  return true;
 }
